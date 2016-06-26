91f9b6645c4a539d01f215d7d4d83415f494d4fc
Soringaming <thehawksquad50@gmail.com>
2016-06-27 06:54:54 +1000

    Added charging, also did the remove method, and seting/removing players gamemode

--- src/me/soringaming/moon/korra/SpiritualProjection/SpiritualProjection.java
+++ src/me/soringaming/moon/korra/SpiritualProjection/SpiritualProjection.java
@@ -2,13 +2,16 @@ package me.soringaming.moon.korra.SpiritualProjection;
 
 import java.util.logging.Level;
 
+import org.bukkit.GameMode;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.permissions.Permission;
 import org.bukkit.permissions.PermissionDefault;
 
 import com.projectkorra.projectkorra.ProjectKorra;
 import com.projectkorra.projectkorra.ability.AddonAbility;
+import com.projectkorra.projectkorra.ability.CoreAbility;
 import com.projectkorra.projectkorra.ability.SpiritualAbility;
 
 public class SpiritualProjection extends SpiritualAbility implements AddonAbility {
@@ -17,12 +20,24 @@ public class SpiritualProjection extends SpiritualAbility implements AddonAbilit
 	private Location loc;
 	private Location start;
 	private Permission perm;
+	private long startTime;
+	private long chargeTime;
+	private long Duration;
+	private GameMode gamemode;
+	private boolean Charged;
+	private boolean Activated;
 	
 	public SpiritualProjection(Player player) {
 		super(player);
 		this.player = player;
 		this.loc = player.getLocation();
 		this.start = player.getLocation();
+		this.startTime = System.currentTimeMillis();
+		this.chargeTime = 5000;
+		this.Charged = false;
+		this.Activated = false;
+		this.gamemode = player.getGameMode();
+		start();
 	}
 
 	@Override
@@ -52,6 +67,43 @@ public class SpiritualProjection extends SpiritualAbility implements AddonAbilit
 
 	@Override
 	public void progress() {
+		if(!bPlayer.getBoundAbility().equals(CoreAbility.getAbility("SpiritualProjection"))) {
+			remove();
+			return;
+		}
+		this.loc = player.getLocation();
+		if(Charged == false) {
+			if(player.isSneaking() && chargeTime + startTime > System.currentTimeMillis()) {
+				
+			}
+			
+			if(player.isSneaking() && chargeTime + startTime < System.currentTimeMillis()) {
+				Charged = true;
+			}
+			
+			if(!player.isSneaking() && chargeTime + startTime > System.currentTimeMillis()) {
+				remove();
+				return;
+			}
+			if(!player.isSneaking() && chargeTime + startTime < System.currentTimeMillis()) {
+				Charged = true;
+			}
+		} else {
+			if(Activated == false) {
+				this.startTime = System.currentTimeMillis();
+				this.Duration = 6000;
+				Activated = true;
+			}
+			if(startTime + Duration < System.currentTimeMillis()) {
+				remove();
+				return;
+			}
+			player.setGameMode(GameMode.SPECTATOR);
+			if(loc.getBlock().getType() == Material.BEDROCK || loc.getBlock().getType() == Material.BARRIER) {
+				remove();
+				return;
+			}
+		}
 		
 	}
 
@@ -72,6 +124,12 @@ public class SpiritualProjection extends SpiritualAbility implements AddonAbilit
 		perm = new Permission("bending.ability.SpiritualProjection");
 		ProjectKorra.plugin.getServer().getPluginManager().addPermission(perm);
 		perm.setDefault(PermissionDefault.TRUE);
+	}
+	
+	@Override
+	public void remove() {
+		player.teleport(start);
+		player.setGameMode(gamemode);
 	}
 
 	@Override
