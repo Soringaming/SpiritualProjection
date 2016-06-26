6627bc1d0f9e6b78d83fe527725c6dfaedeebbc5
Soringaming <thehawksquad50@gmail.com>
2016-06-27 06:16:05 +1000

    Finished the listener

--- src/me/soringaming/moon/korra/SpiritualProjection/SpiritualProjectionListener.java
+++ src/me/soringaming/moon/korra/SpiritualProjection/SpiritualProjectionListener.java
@@ -1,7 +1,25 @@
 package me.soringaming.moon.korra.SpiritualProjection;
 
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerToggleSneakEvent;
+
+import com.projectkorra.projectkorra.BendingPlayer;
+import com.projectkorra.projectkorra.ability.CoreAbility;
 
 public class SpiritualProjectionListener implements Listener {
+	
+	@EventHandler
+	public void toggleSneak(PlayerToggleSneakEvent e) {
+		Player p = e.getPlayer();
+		BendingPlayer bp = BendingPlayer.getBendingPlayer(p);
+		if(p.isSneaking()) {
+			return;
+		}
+		if(bp.canBend(CoreAbility.getAbility("SpiritualProjection"))) {
+			new SpiritualProjection(p);
+		}
+	}
 
 }
