2495352e898fcb9150ebd4c38a7ff354f22a1875
Soringaming <thehawksquad50@gmail.com>
2016-06-27 06:03:50 +1000

    I has done le shiz lewl

---
+++ src/me/soringaming/moon/korra/SpiritualProjection/SpiritualProjection.java
@@ -0,0 +1,76 @@
+package me.soringaming.moon.korra.SpiritualProjection;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+
+import com.projectkorra.projectkorra.ability.AddonAbility;
+import com.projectkorra.projectkorra.ability.SpiritualAbility;
+
+public class SpiritualProjection extends SpiritualAbility implements AddonAbility {
+
+	public SpiritualProjection(Player player) {
+		super(player);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public long getCooldown() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Location getLocation() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isHarmlessAbility() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isSneakAbility() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void progress() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getAuthor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getVersion() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void load() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void stop() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/me/soringaming/moon/korra/SpiritualProjection/SpiritualProjectionListener.java
@@ -0,0 +1,7 @@
+package me.soringaming.moon.korra.SpiritualProjection;
+
+import org.bukkit.event.Listener;
+
+public class SpiritualProjectionListener implements Listener {
+
+}
